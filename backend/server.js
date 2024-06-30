04cf0fe1860ee618c3766a9a7395e233f4dd15fd
Alinda Byamukama <alindabyamukama@gmail.com>
2024-06-30 23:54:52 +0300

    add update to server to use book routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const sequelize = require('./config/database')
 const Book = require('./models/Book')
+const bookRoutes = require('./routes/bookRoutes')
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
+
+app.use('./api', bookRoutes)
 
 app.get('/', (req, res) => {
     res.send('Library Management System API')
