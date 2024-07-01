ebfe5f34ba25d14e38f4556d05d885722311199c
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-02 00:34:24 +0300

    add state variables for emal, password and error

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -1,2 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios'
+
+const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+
+    
+}
