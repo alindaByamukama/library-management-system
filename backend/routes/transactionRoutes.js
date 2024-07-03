bd5077fa8491e26f52331f9ac0168eab88efc1d5
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 00:43:10 +0300

    add a post transaction route to borrow books

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,3 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const Transaction = require('../models/Transaction')
+
+// Borrow a book (create a transaction)
+router.post('/transactions', async (req, res) => {
+    const { bookId, userId } = req.body
+    try {
+        const newTransaction = await Transaction.create({ bookId, userId })
+    } catch (error) {
+        res.status(500).send('Server error')
+    }
+})
+
+module.exports = router
