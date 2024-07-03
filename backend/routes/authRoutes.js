8589fb87fb56c8df887ad3961fee0446e7f3f17f
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 00:34:01 +0300

    add update route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -25,4 +25,25 @@ router.get('/users', async (req, res) => {
     }
 })
 
+// Update a user
+router.put('/users/:id', async (req, res) => {
+    const { id } = req.params
+    const { email, password } = req.body
+    try {
+        const user = await User.findByPk(id)
+        if(user) {
+            user.email = email
+            if (password) {
+                user.password = await bcrypt.hash(password, 10)
+            }
+            await user.save()
+            res.json(user)
+            } else {
+                res.status(404).send('Server error')
+            }
+        } catch (error) {
+        res.status(500).send('Server error')
+    }
+})
+
 module.exports = router
