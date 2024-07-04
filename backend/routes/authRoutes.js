f486a4ab73b186855e9e8d369012d64a3af38b10
Alinda Byamukama <alindabyamukama@gmail.com>
2024-07-04 10:30:39 +0300

    add json web token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 // Register a new user
 router.post('/register', async (req, res) => {
